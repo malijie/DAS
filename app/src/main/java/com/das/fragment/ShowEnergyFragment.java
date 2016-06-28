978d914fb06edbb777dfae9371a92d4a79eb6c6a
malijie <190223629@qq.com>
2016-06-28 22:57:18 +0800

    提交能耗布局

---
+++ app/src/main/java/com/das/fragment/ShowEnergyFragment.java
@@ -0,0 +1,43 @@
+package com.das.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.das.chart.ChartManager;
+import com.example.das.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+
+/**
+ * Created by �� on 2016/4/19.
+ */
+public class ShowEnergyFragment extends Fragment{
+
+    private TextView mTextCurrentSpeed = null;
+    private BarChart mBarChartSuggestSpeed = null;
+    private BarData mBarData;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_showspeed, container,false);
+
+        initViews(v);
+        initData();
+
+        return v;
+    }
+
+    private void initData() {
+        ChartManager.getInstance().createEnergyBarChart(mBarChartSuggestSpeed);
+    }
+
+    private void initViews(View v) {
+        mTextCurrentSpeed = (TextView) v.findViewById(R.id.id_speed_text_current_speed);
+        mBarChartSuggestSpeed = (BarChart)v.findViewById(R.id.id_speed_bar_chart_suggest_speed);
+    }
+
+}
